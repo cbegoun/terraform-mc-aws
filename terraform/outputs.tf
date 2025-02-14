44b14c8b51fea2cba6cc674a7e4695a826420761
cbegoun <begouncody@gmail.com>
2025-02-14 15:29:54 -0700

    outputting task role name and arn

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,11 @@ output "hosted_zone_nameservers" {
   value       = aws_route53_zone.minecraft_ondemand_route53_zone.name_servers
   description = "The Hosted Zone's NS records. Use this to delegate the zone from your parent zone."
 }
+
+output "ecs_task_execution_role_name" {
+  value = aws_iam_role.ecs_task_execution_role.name
+}
+
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
